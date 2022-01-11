42af11143d0be5dfcd2d04faf7dbaaa20612a41f
Ankit Kumar <87308865+ankitrajput30@users.noreply.github.com>
2022-01-11 23:35:05 +0530

    Add files via upload

---
+++ src/main/java/com/example/basiccalculator/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.basiccalculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edFirstValue, edSecondValue;
+    TextView tvAns;
+    Button add, subtract, multiply, divide;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edFirstValue = findViewById(R.id.edFirstValue);
+        edSecondValue = findViewById(R.id.edSecondValue);
+
+        tvAns = findViewById(R.id.tvAns);
+
+        add = findViewById(R.id.btnAdd);
+        subtract = findViewById(R.id.btnSubtract);
+        multiply = findViewById(R.id.btnMultiply);
+        divide = findViewById(R.id.btnDivide);
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int firstValue, secondValue, ans;
+
+                firstValue = Integer.parseInt(edFirstValue.getText().toString());
+                secondValue = Integer.parseInt(edSecondValue.getText().toString());
+
+                ans = firstValue + secondValue;
+
+                tvAns.setText("Ans is = "+ ans);
+
+            }
+        });
+
+        subtract.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int firstValue, secondValue, ans;
+
+                firstValue = Integer.parseInt(edFirstValue.getText().toString());
+                secondValue = Integer.parseInt(edSecondValue.getText().toString());
+
+                ans = firstValue - secondValue;
+
+                tvAns.setText("Ans is = "+ ans);
+
+            }
+        });
+
+        multiply.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int firstValue, secondValue, ans;
+
+                firstValue = Integer.parseInt(edFirstValue.getText().toString());
+                secondValue = Integer.parseInt(edSecondValue.getText().toString());
+
+                ans = firstValue * secondValue;
+
+                tvAns.setText("Ans is = "+ ans);
+
+            }
+        });
+
+        divide.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int firstValue, secondValue, ans;
+
+                firstValue = Integer.parseInt(edFirstValue.getText().toString());
+                secondValue = Integer.parseInt(edSecondValue.getText().toString());
+
+                ans = firstValue / secondValue;
+
+                tvAns.setText("Ans is = "+ ans);
+
+            }
+        });
+    }
+}
